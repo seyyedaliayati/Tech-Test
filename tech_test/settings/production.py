83848a4b4c766bf9946190c0e8df2ab35543d62f
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2021-07-05 21:49:23 +0430

    Refactored Code

---
+++ tech_test/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['example.com', ]
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# ADD PRODUCTION DATABASE SETTINGS HERE
+
